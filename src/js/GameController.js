3714a5169a0eb7e42914f817f4f7c1667f259384
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2021-05-30 13:05:35 +0300

    step 7

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -11,9 +11,6 @@ export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
-
-    this.userTeamWithPositions = [];
-    this.computerTeamWithPositions = [];
   }
 
   init() {
@@ -38,6 +35,16 @@ export default class GameController {
     const userTeam = generateTeam(new Team().userTeam, 1, 2);
     const computerTeam = generateTeam(new Team().computerTeam, 1, 2);
 
+    this.userTeamWithPositions = [];
+    this.computerTeamWithPositions = [];
+
+    this.userPositions = [
+      0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57,
+    ];
+    this.computerPositions = [
+      6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63,
+    ];
+
     this.setPositions(userTeam, computerTeam);
     this.gamePlay.redrawPositions([
       ...this.userTeamWithPositions,
@@ -46,36 +53,37 @@ export default class GameController {
   }
 
   setPositions(userTeam, computerTeam) {
-    const userPositions = [
-      0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57,
-    ];
-    const computerPositions = [
-      6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63,
-    ];
-
     this.userTeamWithPositions.push(
       new PositionedCharacter(
         userTeam[0],
-        userPositions[Math.floor(Math.random() * userPositions.length)]
+        this.userPositions[
+          Math.floor(Math.random() * this.userPositions.length)
+        ]
       )
     );
     this.userTeamWithPositions.push(
       new PositionedCharacter(
         userTeam[1],
-        userPositions[Math.floor(Math.random() * userPositions.length)]
+        this.userPositions[
+          Math.floor(Math.random() * this.userPositions.length)
+        ]
       )
     );
 
     this.computerTeamWithPositions.push(
       new PositionedCharacter(
         computerTeam[0],
-        computerPositions[Math.floor(Math.random() * computerPositions.length)]
+        this.computerPositions[
+          Math.floor(Math.random() * this.computerPositions.length)
+        ]
       )
     );
     this.computerTeamWithPositions.push(
       new PositionedCharacter(
         computerTeam[1],
-        computerPositions[Math.floor(Math.random() * computerPositions.length)]
+        this.computerPositions[
+          Math.floor(Math.random() * this.computerPositions.length)
+        ]
       )
     );
   }
